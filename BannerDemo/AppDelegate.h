10bb436fdf1a5f012e083743a5fa46976f36439f
树振郭 <shuzhenguo@macBook-Pro.local>
2016-09-21 16:54:48 +0800

    Initial Commit

---
+++ BannerDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BannerDemo
+//
+//  Created by shuzhenguo on 2016/9/21.
+//  Copyright © 2016年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BannerDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BannerDemo
+//
+//  Created by shuzhenguo on 2016/9/21.
+//  Copyright © 2016年 shuzhenguo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
