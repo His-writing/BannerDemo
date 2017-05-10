4968b8aa9345efe48fb02fcde39f52d9b7cb9b9a
shuzhenguo <shuzhenguovip@163.com>
2017-05-10 19:51:38 +0800

    滚动的视图
    滚动的视图

--- BannerDemo/ViewController.m
+++ BannerDemo/ViewController.m
@@ -7,8 +7,9 @@
 //
 
 #import "ViewController.h"
+#import "SDCycleScrollView.h"
 
-@interface ViewController ()
+@interface ViewController ()<SDCycleScrollViewDelegate>
 
 @end
 
@@ -17,7 +18,54 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    // 情景二：采用网络图片实现
+//    NSArray *imagesURLStrings = @[
+//                                  @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg",
+//                                  @"http://c.hiphotos.baidu.com/image/w%3D400/sign=c2318ff84334970a4773112fa5c8d1c0/b7fd5266d0160924c1fae5ccd60735fae7cd340d.jpg"
+//
+//                                  ];
+//    
+//    
+//    
+//    // 情景三：图片配文字
+//    NSArray *titles = @[@"新建交流QQ群：185534916"];
+//
+    NSArray *imagesURLStrings = @[
+                                  
+                                  ];
+    
+    
+    
+    // 情景三：图片配文字
+    NSArray *titles = @[];
+    
+    
+
+    
+    // 网络加载 --- 创建带标题的图片轮播器
+    SDCycleScrollView *cycleScrollView2 = [SDCycleScrollView cycleScrollViewWithFrame:CGRectMake(0, 0, 320, 180) delegate:self placeholderImage:[UIImage imageNamed:@"placeholder"]];
+    
+    cycleScrollView2.pageControlAliment = SDCycleScrollViewPageContolAlimentRight;
+    cycleScrollView2.titlesGroup = titles;
+    cycleScrollView2.currentPageDotColor = [UIColor whiteColor]; // 自定义分页控件小圆标颜色
+    [self.view addSubview:cycleScrollView2];
+    
+    //         --- 模拟加载延迟
+    dispatch_after(dispatch_time(DISPATCH_TIME_NOW, (int64_t)(0.3 * NSEC_PER_SEC)), dispatch_get_main_queue(), ^{
+        cycleScrollView2.imageURLStringsGroup = imagesURLStrings;
+    });
+    
+
+
 }
+
+
+- (void)cycleScrollView:(SDCycleScrollView *)cycleScrollView didSelectItemAtIndex:(NSInteger)index
+{
+    NSLog(@"---点击了第%ld张图片", (long)index);
+    
+}
+
 
 
 - (void)didReceiveMemoryWarning {
